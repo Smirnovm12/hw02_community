922073a91454870151388eee55118251122dd8a3
Smirnovm12 <smirnovm12@gmail.com>
2021-07-06 16:42:17 +0300

    1.1

--- posts/admin.py
+++ posts/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import  Group, Post
 
-
-class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
-    list_display = ("text", "pub_date", "author")
-    # добавляем интерфейс для поиска по тексту постов
-    search_fields = ("text",)
-    # добавляем возможность фильтрации по дате
-    list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"
-# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
-admin.site.register(Post, PostAdmin)
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Указываем поля, которые будут отображаться в Админке
+    list_display = ("pk", "text", "pub_date", "author")
+
+# Добавляем интерфейс для поиска данных
+    search_fields = ("text",)
+# Добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",)
+# это свойство сработает для всех колонок: где пусто - там будет эта строка
+    empty_value_display = "-пусто-"
+
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("title", "slug")
+    empty_value_display = "-пусто-"
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Group
+
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:10]
@@ -8,5 +9,15 @@ def index(request):
     for item in latest:
         output.append(item.text)
     return HttpResponse('\n'.join(output))
+
+def group_posts(request, slug):
+    # функция get_object_or_404 получает по заданным критериям объект из базы данных 
+    # или возвращает сообщение об ошибке, если объект не найден
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
 
 # Create your views here.
